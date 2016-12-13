19df97691bd59504abaa86d41df0013aa97946a7
sQ` <github@fnord.com.ar>
2016-12-12 23:31:43 -0300

    first

---
+++ raid_check.1h.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo 'RAID'
+echo '---'
+diskutil appleraid list | awk '/^Status/ {print "apple:",$2}'
+diskutil appleraid list | awk '/^[01]/ {print "--"$2,$4}'
+echo '---'
+/usr/local/bin/zpool status    tank | awk '/state:/    {print "zfs:",$2}'
+/usr/local/bin/zpool status -L tank | awk '/disk[0-9]/ {print "--"$1,$2}'
+
+
