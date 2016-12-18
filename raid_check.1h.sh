8aa7a4d89f20086f571b8618a8eae2fdc1af84e2
sQ` <github@fnord.com.ar>
2016-12-18 11:14:03 -0300

    changed names back

--- raid_check.1h.sh
+++ raid_check.1h.sh
@@ -2,8 +2,8 @@
 
 echo '💾'
 echo '---'
-diskutil appleraid list | awk '/^Status/ {print "apple:",$2}'
+diskutil appleraid list | awk '/^Status/ {print "mirror:",$2}'
 diskutil appleraid list | awk '/^[01]/ {print "--"$2,$4}'
 echo '---'
-/usr/local/bin/zpool status    tank | awk '/state:/    {print "zfs:",$2}'
-/usr/local/bin/zpool status -L tank | awk '/disk[0-9]/ {print "--"$1,$2}'
+/usr/local/bin/zpool status    tank | awk '/state:/    {print "tank:",$2}'
+/usr/local/bin/zpool status -L tank | awk '/disk[0-9]/ {print "--"$1, $2}'
